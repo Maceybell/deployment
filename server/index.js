c86974c4f3da58ad44746efdb5241c541f7354fc
Macey Bell <maceytbell@gmail.com>
2022-11-02 12:32:45 -0600

    invoked json

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-app.use(express.json)
+app.use(express.json())
 app.use(cors())
 
 const {home} = require("./controllers/pageCtrl")
